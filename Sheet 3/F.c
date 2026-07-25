5e34d9cb970ba1f84135c029d770798fe22d4527
Mohammed El-Ahmady <mohamedelahmady08@gmail.com>
2026-07-25 17:32:17 +0300

    Problem F done

---
+++ Sheet 3/F.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+  int n = 0;
+  scanf("%d", &n);
+  long long arr[n];
+  for (int i = 0; i < n; i++) {
+    scanf("%lld", &arr[i]);
+  }
+  for (int i = n - 1; i >= 0; i--) {
+    printf("%lld ", arr[i]);
+  }
+}
